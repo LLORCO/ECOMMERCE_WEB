dccaa14def8174df4ef87c8213cf01a639b35e71
Jorge Luis <jlhc1996@gmail.com>
2024-08-28 18:11:01 -0600

    Best products section added, scroll animation to products section added.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,47 +1,52 @@
 import React from "react";
+import Img1 from "../../assets/women/women.png";
+import Img2 from "../../assets/women/women2.jpg";
+import Img3 from "../../assets/women/women3.jpg";
+import Img4 from "../../assets/women/women4.jpg";
+import { FaStar } from "react-icons/fa6";
 
 const ProductsData = [
-    {
-        id:1,
-        img: Img1,
-        title: "Women Ethnic",
-        rating: 5.0,
-        author: "white",
-        aosDelay: "0",
-    },
-    {
-        id:2,
-        img: Img2,
-        title: "Women estern",
-        rating: 4.5,
-        author: "Red",
-        aosDelay: "200",
-    },
-    {
-        id:3,
-        img: Img3,
-        title: "Goggles",
-        rating: 4.7,
-        author: "brown",
-        aosDelay: "400",
-    },
-    {
-        id:4,
-        img: Img4,
-        title: "Printed T-Shirt",
-        rating: 4.4,
-        author: "yellow",
-        aosDelay: "600",
-    },
-    {
-        id:5,
-        img: Img5,
-        title: "Fasion T-Shirt",
-        rating: 4.5,
-        author: "Pink",
-        aosDelay: "800",
-    },
-]
+  {
+    id: 1,
+    img: Img1,
+    title: "Women Ethnic",
+    rating: 5.0,
+    color: "white",
+    aosDelay: "0",
+  },
+  {
+    id: 2,
+    img: Img2,
+    title: "Women estern",
+    rating: 4.5,
+    color: "Red",
+    aosDelay: "200",
+  },
+  {
+    id: 3,
+    img: Img3,
+    title: "Goggles",
+    rating: 4.7,
+    color: "brown",
+    aosDelay: "400",
+  },
+  {
+    id: 4,
+    img: Img4,
+    title: "Printed T-Shirt",
+    rating: 4.4,
+    color: "yellow",
+    aosDelay: "600",
+  },
+  {
+    id: 5,
+    img: Img2,
+    title: "Fasion T-Shirt",
+    rating: 4.5,
+    color: "Pink",
+    aosDelay: "800",
+  },
+];
 
 export const Products = () => {
   return (
@@ -49,9 +54,9 @@ export const Products = () => {
       <div className="container">
         {/* Header section */}
         <div className="text-center mb-10 max-w-[600px] mx-auto">
-          <p className="text-sm text-primary">Top Selling Products</p>
-          <h1 className="text-3xl font-bold">Products</h1>
-          <p className="text-xs text-gray-400">
+          <p data-aos="fade-up" className="text-sm text-primary">Top Selling Products for you</p>
+          <h1 data-aos="fade-up" className="text-3xl font-bold">Products</h1>
+          <p data-aos="fade-up" className="text-xs text-gray-400">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia
             quidem dignissimos et velit adipisci
           </p>
@@ -60,8 +65,27 @@ export const Products = () => {
         <div>
           <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5">
             {/* Card section */}
-            {ProductsData.map(()=>(
-                <div></div>
+            {ProductsData.map((data) => (
+              <div 
+              data-aos="fade-up"
+              data-aos-delay={data.aosDelay}
+              key={data.id} 
+              className="space-y-3"
+              >
+                <img
+                  src={data.img}
+                  alt=""
+                  className="h-[220px] w-[150px] object-cover rounded-md"
+                />
+                <div>
+                  <h3 className="font-semibold">{data.title}</h3>
+                  <p className="text-sm text-gray-600">{data.color}</p>
+                  <div className="flex items-center gap-1">
+                    <FaStar className="text-yellow-400"/>
+                    <span>{data.rating}</span>
+                  </div>
+                </div>
+              </div>
             ))}
           </div>
         </div>
